152c3f88b371b87cf340d2bae039e2dd3a7c3658
wbejobw <www.bejob.ma@gmail.com>
2026-07-16 03:20:32 +0100

    style(ui): Replace PNG logo with responsive CSS text logo

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function BrandLogo({ onClick }) {
   return (
@@ -9,16 +8,10 @@ export default function BrandLogo({ onClick }) {
       className="brand-logo interactive-scale" 
       onClick={onClick} 
       aria-label="Accueil BeClean"
-      style={{ display: 'inline-block', position: 'relative' }}
     >
-      <Image 
-        src="/logo-be.png" 
-        alt="BeClean Logo" 
-        width={140} 
-        height={40} 
-        style={{ objectFit: 'contain' }}
-        priority
-      />
+      <span className="text-logo">
+        BeClean
+      </span>
     </Link>
   );
 }
